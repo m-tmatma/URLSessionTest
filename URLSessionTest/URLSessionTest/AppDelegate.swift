67916461bb464c59c512d5c112694388905faaee
Masaru Tsuchiyama <m.tmatma@gmail.com>
2016-12-11 14:42:00 +0900

    URLSession のテストプログラムを追加する

---
+++ URLSessionTest/URLSessionTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  URLSessionTest
+//
+//  Created by tsuchiyamamasaru on 2016/12/11.
+//  Copyright © 2016年 tsuchiyamamasaru. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ URLSessionTest/URLSessionTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  URLSessionTest
+//
+//  Created by tsuchiyamamasaru on 2016/12/11.
+//  Copyright © 2016年 tsuchiyamamasaru. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
